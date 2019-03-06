318d5a24213b626bf29e61acbcd33b42764da7d4
bertw2002 <awan00@stuy.edu>
2019-03-06 11:45:45 -0500

    some code

--- USACO.java
+++ USACO.java
@@ -1,6 +1,10 @@
 public class USACO{
-  public static int bronze(String filename);
+  public static int bronze(String filename){
+    File file = new File(filename);
+    Scanner scanner = new Scanner(file);
+    
+  }
   public static int silver(String filename);
-  
+
 
 }
