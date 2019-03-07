d6975309e798ca6b11c0d0b4247b89f5b2ad38bd
bertw2002 <awan00@stuy.edu>
2019-03-07 11:47:40 -0500

    parseint

--- USACO.java
+++ USACO.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 public class USACO{
-  public static int bronze(String filename){
+  public static int bronze(String filename) throws FileNotFoundException{
     File file = new File(filename);
     Scanner scanner = new Scanner(file);
-    
+    int R = Integer.parseInt(scanner.next());
+    int C = Integer.parseInt(scanner.next());
+    int E = Integer.parseInt(scanner.next());
+    int N = Integer.parseInt(scanner.next());
+    int[][] lake =  new int[R][C];
+    for (int x = 0; x < lake.length; x++){
+      for (int y = 0; y < lake[0].length; y++){
+        data[x][y] = Integer.parseInt(scanner.next());
+      }
+    }
+    for (int x = )
+    return 0;
   }
-  public static int silver(String filename);
+  //public static int silver(String filename);
 
 
 }
