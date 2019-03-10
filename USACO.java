d253c77d8b3723a2d027aa4c1a4cd52cf8069aee
bertw2002 <awan00@stuy.edu>
2019-03-10 14:09:56 -0400

    finished silver, havent tested

--- USACO.java
+++ USACO.java
@@ -47,25 +47,89 @@ public class USACO{
           }
         }
       }
-      int sum = 0;
+      //calculate volume
+      int depth = 0;
       for(int x = 0; x < R; x++){
         for(int y = 0; y < C; y++){
         	if(lake[x][y] < E){
-        		sum += E - lake[x][y];
+        		depth += E - lake[x][y];
         	}
         }
       }
-      return sum * 72 * 72;
+      return 72 * 72 * depth;
     }
     catch(FileNotFoundException e){System.out.println("bad filename");return -100000;}
   }
-  //public static int silver(String filename);
+
+  public static int silver(String filename){
+    try{
+      //set up local variables
+      File file = new File(filename);
+      Scanner scanner = new Scanner(file);
+      int N = Integer.parseInt(scanner.next());
+      int M = Integer.parseInt(scanner.next());
+      int T = Integer.parseInt(scanner.next());
+      //initialize pasture.
+      String[][] pasture =  new String[N][M];
+      for (int x = 0; x < pasture.length; x++){
+        for (int y = 0; y < pasture[0].length; y++){
+          pasture[x][y] = scanner.next();
+        }
+      }
+      //instructions.
+      int[] instr = new int[4];
+      for (int x = 0; x < instr.length; x++) {
+        instr[x] = scanner.nextInt();
+      }
+      //do instructions.
+      int[][] newpasture = new int[N][M];
+      for (int x = 0; x < pasture.length; x++){
+        for (int y = 0; y < pasture[0].length; y++){
+          if (pasture[x][y].equals("*")){
+            newpasture[x][y] = -1;
+          }else{
+            newpasture[x][y] = 0;
+          }
+        }
+      }
+      newpasture[instr[0]][instr[1]] = 1;
+
+      for (int time = 0; time < T; time++){
+        for (int x = 0; x < newpasture.length; x++) {
+          for (int y = 0; y < newpasture[x].length; y++) {
+            if (newpasture[x][y] != -1) {
+              newpasture[x][y] = 0;
+
+              if (y + 1 < newpasture[x].length && newpasture[x][y + 1] >= 0) {
+                newpasture[x][y] += newpasture[x][y + 1];
+              }
+
+              if (y - 1 >= 0 && newpasture[x][y - 1] >= 0) {
+                newpasture[x][y] += newpasture[x][y - 1];
+              }
+
+              if (x - 1 >= 0 && newpasture[x - 1][y] >= 0) {
+                newpasture[x][y] += newpasture[x - 1][y];
+              }
+
+              if (x + 1 < newpasture.length && newpasture[x + 1][y] >= 0) {
+                newpasture[x][y] += newpasture[x + 1][y];
+              }
+
+            }
+          }
+        }
+      }
+      return newpasture[instr[2]][instr[3]];
+    }
+    catch(FileNotFoundException e) {System.out.println("bad filename");return -100000;}
+  }
   public static void main(String[] args) {
        System.out.println("Bronze\n");
        System.out.println(USACO.bronze("test1.txt"));
-       System.out.println("should be 342144.\n");
+       System.out.println("should be 342144\n");
        System.out.println(USACO.bronze("test2.txt"));
-       System.out.println("Correct answer is 102762432.\n");
+       System.out.println("should be 102762432\n");
        }
 
 
